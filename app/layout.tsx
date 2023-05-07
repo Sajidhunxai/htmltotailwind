746b87c1f28364856e9245a06e471b028e2aa86a
sajid wali <sajidhunxaimaze@gmail.com>
2023-05-07 23:16:26 +0500

    small change in meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ import Footer from './components/Footer';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Web Design &amp; Development Agency | SquareNetMedia',
+  title: 'Web Design & Development Agency | SquareNetMedia',
   description: 'At SquareNet Media, we provide the full team: SEO, Web Design, Graphic Design, Web Development, and many other marketing services for you to explore.',
 }
 
